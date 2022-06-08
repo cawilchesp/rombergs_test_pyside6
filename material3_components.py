ffe8fb5471de23c2ab1432177a5723fa9d8fbd66
Carlos Andrés Wilches Pérez <cawilchesp@outlook.com>
2022-06-08 17:15:17 -0500

    Update material3_components.py
    Added component: Date Field

--- material3_components.py
+++ material3_components.py
@@ -758,7 +758,78 @@ class TextField(QtWidgets.QFrame):
         elif language == 1: self.label_field.setText(self.label_en)
         self.label_field.adjustSize()
 
+# ----------
+# Date Field
+# ----------
+class DateField(QtWidgets.QFrame):
+    def __init__(self, parent, geometry: tuple, labels: tuple, theme: bool, language: int) -> None:
+        """ Material Design 3 Component: Date Field
 
+        Parameters
+        ----------
+        geometry: tuple
+            Date field position and width
+            (x, y, w) -> x, y: upper left corner, w: width
+        labels: tuple
+            Date field text
+            (label_es, label_en) -> label_es: label in spanish, label_en: label in english
+        theme: bool
+            App theme
+            True: Light theme, False: Dark theme
+        language: int
+            App language
+            0: Spanish, 1: English
+        
+        Returns
+        -------
+        None
+        """
+        super(DateField, self).__init__(parent)
+
+        self.label_es, self.label_en = labels
+        x, y, w = geometry
+
+        self.setGeometry(x, y, w, 52)
+
+        self.text_field = QtWidgets.QDateEdit(self)
+        self.text_field.setGeometry(0, 8, w, 44)
+        self.text_field.setCalendarPopup(True)
+        self.text_field.setFrame(False)
+        self.text_field.setSpecialValueText('')
+        self.text_field.setDate(QtCore.QDate.currentDate())
+        
+        self.label_field = QtWidgets.QLabel(self)
+        self.label_field.setGeometry(8, 0, 16, 16)
+        self.label_field.setFont(QtGui.QFont('Segoe UI', 9))
+
+        self.apply_styleSheet(theme)
+        self.language_text(language)
+
+    def apply_styleSheet(self, theme: bool) -> None:
+        """ Apply theme style sheet to component """
+        if theme:
+            background_color = light["surface"]
+            color = light["on_surface"]
+            drop_background_color = light["primary"]
+        else:
+            background_color = dark["surface"]
+            color = dark["on_surface"]
+            drop_background_color = dark["primary"]
+        self.setStyleSheet(f'QLabel {{ border: 0px solid; padding: 0 4 0 4;'
+                f'background-color: {background_color}; color: {color} }}'
+                f'QDateEdit {{ border: 1px solid {color}; border-radius: 4; padding: 0 8 0 8;'
+                f'background-color: {background_color}; color: {color}; }}'
+                f'QDateEdit::drop-down {{ background-color: {drop_background_color};'
+                f'width: 32; height: 32; subcontrol-position: center right; left: -4;'
+                f'border-radius: 16 }}'
+                f'QDateEdit::down-arrow {{ image: url({images_path}/calendar_L.png);'
+                f'width: 16; height: 16 }}')
+
+    def language_text(self, language: int) -> None:
+        """ Change language of label text  """
+        if language == 0:   self.label_field.setText(self.label_es)
+        elif language == 1: self.label_field.setText(self.label_en)
+        self.label_field.adjustSize()
 
 
 
